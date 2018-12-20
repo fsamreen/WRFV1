a8175a8347feb03a4324ac43d73cc1c42cc8cbe2
fsamreen <aziaf2002@yahoo.com>
2018-12-20 11:34:29 +0000

    Update install1f.sh

--- install1f.sh
+++ install1f.sh
@@ -176,9 +176,9 @@ gunzip WRFV3.9.1.1.TAR.gz
 tar -xf WRFV3.9.1.1.TAR
 echo " Download WPS......"
 cd ~/Build_WRF
-wget http://www2.mmm.ucar.edu/wrf/src/WPSV3.9.TAR.gz .
-gunzip WPSV3.9.TAR.gz
-tar -xvf WPSV3.9.TAR
+wget http://www2.mmm.ucar.edu/wrf/src/WPSV3.9.1.TAR.gz .
+gunzip WPSV3.9.1.TAR.gz
+tar -xvf WPSV3.9.1.TAR
 #cd WPS
 #./configure
 cd ~/Build_WRF
